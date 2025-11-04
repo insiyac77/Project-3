3de1f51e2d5cef2dc4c6321db50f8a87fd64a896
parikhro <100094364+parikhro@users.noreply.github.com>
2025-11-03 22:42:15 -0500

    added test files, did q2 and q3

--- analysis.py
+++ analysis.py
@@ -22,41 +22,56 @@
 
 def question2():
     answerDiscount = 0.9
-    answerNoise = 0.2
+    # changing the noise to 0 will guarantee the agent crosses as it will never fall off the bridge
+    answerNoise = 0
     return answerDiscount, answerNoise
 
 def question3a():
-    answerDiscount = None
-    answerNoise = None
-    answerLivingReward = None
+    # prefer the close exit and risking cliff
+    # we want a myopic (low discount) factor with low noise (will be unlikely to take high path)
+    # and a negative living reward to make it extra myopic (knows its dying)
+    answerDiscount = 0.1
+    answerNoise = 0
+    answerLivingReward = -1
     return answerDiscount, answerNoise, answerLivingReward
     # If not possible, return 'NOT POSSIBLE'
 
 def question3b():
-    answerDiscount = None
-    answerNoise = None
-    answerLivingReward = None
+    # prefer close exit but avoid cliff
+    # still want myopic but now we should increase noise to dissuade taking the cliff path
+    # also don't want too myopic or it will prioritize the close one too much
+    # still want a negative living reward
+    answerDiscount = 0.3
+    answerNoise = 0.3
+    answerLivingReward = -1
     return answerDiscount, answerNoise, answerLivingReward
     # If not possible, return 'NOT POSSIBLE'
 
 def question3c():
-    answerDiscount = None
-    answerNoise = None
-    answerLivingReward = None
+    # prefer far exit and risk cliff
+    # want far sighted, but low noise
+    # and positive living reward to encourage exiting later
+    answerDiscount = 0.8
+    answerNoise = 0
+    answerLivingReward = 1
     return answerDiscount, answerNoise, answerLivingReward
     # If not possible, return 'NOT POSSIBLE'
 
 def question3d():
-    answerDiscount = None
-    answerNoise = None
-    answerLivingReward = None
+    # prefer far exit but avoid cliff
+    # want far sighted, noise, positive living reward
+    answerDiscount = 0.8
+    answerNoise = 0.3
+    answerLivingReward = 1
     return answerDiscount, answerNoise, answerLivingReward
     # If not possible, return 'NOT POSSIBLE'
 
 def question3e():
-    answerDiscount = None
-    answerNoise = None
-    answerLivingReward = None
+    # avoid both exits and cliff
+    # want high discount, extremely high noise, negative living reward
+    answerDiscount = 0.5
+    answerNoise = 0.9
+    answerLivingReward = -1
     return answerDiscount, answerNoise, answerLivingReward
     # If not possible, return 'NOT POSSIBLE'
 
